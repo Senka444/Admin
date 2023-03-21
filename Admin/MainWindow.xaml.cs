0128b1435ddfab977c8eeef5f6c2180a3179bfe7
DakitaNorth <garryant3120@yandex.ru>
2023-03-21 12:29:20 +0300

    Add project files.

---
+++ Admin/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Admin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            /*if (textBox4.Text == "Введите секретное слово")
+                textBox4.Clear();*/
+        }
+
+        private void textBox4_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (textBox4.Text == "Введите секретное слово")
+            {
+                textBox4.Clear();
+            }
+            
+        }
+
+        private void textBox4_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (textBox4.Text != null)
+            {
+                textBox4.Text = "Введите секретное слово";
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
